2185ebbcc96ba1e64732511efa267c1027c287a7
bigTo <aaa>
2019-03-21 16:39:06 +0800

    添加了“关于本网页”的页面
    添加了........................

--- wordcount/function.py
+++ wordcount/function.py
@@ -22,3 +22,5 @@ def count(request):
     return render(request, 'count.html',{'count':total_count,
                                          "text":user_text,
                                          "sorted":sorted_dict})
+def about(request):
+    return render(request, 'about.html')
